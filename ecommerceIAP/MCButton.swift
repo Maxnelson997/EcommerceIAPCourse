3a637b243259d5ec55680297945e28fa43b84f27
Max <maxnelson997@gmail.com>
2019-06-19 20:44:58 -0600

    add a dropshadow to any uiview

--- ecommerceIAP/MCButton.swift
+++ ecommerceIAP/MCButton.swift
@@ -12,8 +12,18 @@ class MCButton: UIButton {
     init(text: String, width: CGFloat = 108, height: CGFloat = 45, hasShadow: Bool = true) {
         super.init(frame: .zero)
         
+        hasShadow ? setupShadow(width, height) : ()
         setupConstraints(width, height)
         setupStyles(text)
+    }
+    
+    fileprivate func setupShadow(_ width: CGFloat, _ height: CGFloat) {
+        let shadowSize: CGFloat = 20
+        let contactRect = CGRect(x: 0, y: height - (shadowSize * 0.6), width: width, height: shadowSize)
+        layer.shadowPath = UIBezierPath(ovalIn: contactRect).cgPath
+        layer.shadowRadius = 10
+        layer.shadowOpacity = 0.8
+        layer.shadowColor = UIColor.red.cgColor
     }
     
     fileprivate func setupConstraints(_ width: CGFloat,_ height: CGFloat) {
